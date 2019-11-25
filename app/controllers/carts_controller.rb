edc4b8fef6d2ca5a43f29920a9de9a7d96f90e66
Azzerty23 <azzerty23@hotmail.fr>
2019-11-25 19:06:04 +0100

    add carts_controller show

---
+++ app/controllers/carts_controller.rb
@@ -0,0 +1,32 @@
+class CartsController < ApplicationController
+  # before_action :authenticate_user!
+  before_action :set_cart, only: [:show, :edit]
+
+  def show
+
+    @global_amount = @cart.items.inject(0) {|sum, price| sum+price }
+
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  private
+
+  # def cart_params
+  #   params.require(:cart).permit(:user_id)
+  # end
+
+  def set_cart
+    @cart = Cart.find_by(user_id: current_user.id)
+  end
+
+
+
+end

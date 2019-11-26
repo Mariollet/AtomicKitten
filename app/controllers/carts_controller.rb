7bd7b21ae4b18e990b4832b2def28e60cdab60d6
leovanel <leovanel>
2019-11-26 20:03:05 +0100

    aditem

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -19,10 +19,12 @@ class CartsController < ApplicationController
     @item = Item.find_by(params[:id])
     @additem = CartItem.new(item_id:@item.id, cart_id: @cart.id)
     if @additem.save # essaie de sauvegarder en base @gossip
-          
+      flash[:notice] = "item sauvegardé"    
       redirect_to '/' # si ça marche, il redirige vers la page d'index du site
+      
     else
-      redirect_to item_path(@item.id)  # sinon, il render la view new (qui est celle sur laquelle on est déjà)
+      flash[:notice] = "echec"
+      redirect_to '/'  # sinon, il render la view new (qui est celle sur laquelle on est déjà)
     end
   
  

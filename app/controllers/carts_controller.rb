614433f861f8548ecf32b080371ab800996185e6
mariollet <mariollet@outlook.fr>
2019-11-26 20:23:08 +0100

    change views profil

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,5 +1,5 @@
 class CartsController < ApplicationController
-  # before_action :authenticate_user!
+  before_action :authenticate_user!
   before_action :set_cart, only: [:index, :edit, :update]
 
   def index

72f8b6f9a7cc6a30e1904afb6a8e0288a3937159
Anthonyph <anthony.phim@gmail.com>
2019-11-26 15:32:15 +0100

    mailer test

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  after_create :welcome_send
+  #after_create :welcome_send
 
   has_one :cart
   has_many :cart_items, through: :cart
@@ -13,5 +13,6 @@ class User < ApplicationRecord
   def welcome_send
     UserMailer.welcome_email(self).deliver_now
   end
+  
        
 end

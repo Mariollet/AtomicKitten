212d5888d293c727a95e438139fa481c865190aa
mariollet <mariollet@outlook.fr>
2019-11-27 19:39:52 +0100

    Add stripe

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,5 @@ Rails.application.routes.draw do
   # get '/mon_panier', to: 'carts#show'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :charges
 end

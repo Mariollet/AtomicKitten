6af9924e8d36f07ff921b7927547beecf4a23668
Anthonyph <anthony.phim@gmail.com>
2019-11-27 10:20:14 +0100

    MAILER END - sent email to admin and customer ok

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,14 +2,5 @@ class Order < ApplicationRecord
   belongs_to :user
   has_many :order_items
   has_many :items, through: :order_items
-  
-  
-  after_create :admin_send
-
-
-
-  def admin_send
-    OrderMailer.order_send_admin_email(self).deliver_now
-  end
 
 end

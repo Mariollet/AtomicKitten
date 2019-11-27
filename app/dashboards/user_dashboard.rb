947b95627565a0ecb1e0183adbbb65958fad3829
Azzerty23 <azzerty23@hotmail.fr>
2019-11-27 18:44:11 +0100

    customize users dashboard attributes

--- app/dashboards/user_dashboard.rb
+++ app/dashboards/user_dashboard.rb
@@ -8,11 +8,13 @@ class UserDashboard < Administrate::BaseDashboard
   # which determines how the attribute is displayed
   # on pages throughout the dashboard.
   ATTRIBUTE_TYPES = {
+    id: Field::Number,
+    first_name: Field::String,
+    last_name: Field::String,
     cart: Field::HasOne,
     cart_items: Field::HasMany,
     items: Field::HasMany,
     orders: Field::HasMany,
-    id: Field::Number,
     email: Field::String,
     encrypted_password: Field::String,
     reset_password_token: Field::String,
@@ -20,8 +22,6 @@ class UserDashboard < Administrate::BaseDashboard
     remember_created_at: Field::DateTime,
     created_at: Field::DateTime,
     updated_at: Field::DateTime,
-    first_name: Field::String,
-    last_name: Field::String,
     is_admin: Field::Boolean,
   }.freeze
 
@@ -31,29 +31,31 @@ class UserDashboard < Administrate::BaseDashboard
   # By default, it's limited to four items to reduce clutter on index pages.
   # Feel free to add, remove, or rearrange items.
   COLLECTION_ATTRIBUTES = %i[
-  cart
-  cart_items
-  items
+  id
+  first_name
+  last_name
+  email
   orders
+  created_at
+  is_admin
   ].freeze
 
   # SHOW_PAGE_ATTRIBUTES
   # an array of attributes that will be displayed on the model's show page.
   SHOW_PAGE_ATTRIBUTES = %i[
+  id
+  first_name
+  last_name
   cart
   cart_items
   items
   orders
-  id
   email
   encrypted_password
   reset_password_token
   reset_password_sent_at
   remember_created_at
   created_at
-  updated_at
-  first_name
-  last_name
   is_admin
   ].freeze
 
@@ -90,7 +92,7 @@ class UserDashboard < Administrate::BaseDashboard
   # Overwrite this method to customize how users are displayed
   # across all pages of the admin dashboard.
   #
-  # def display_resource(user)
-  #   "User ##{user.id}"
-  # end
+  def display_resource(user)
+    "##{user.id} #{user.first_name} #{user.last_name}"
+  end
 end
